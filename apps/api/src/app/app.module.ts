9a29570a5c9cdbebb9b32cc60e75ecfe9a7cbc44
Cristi Ratoi <ratoi_crysty@yahoo.com>
2026-02-07 20:15:19 +0200

    refactor: chore cleanup

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ShellModule } from '@home-tools/shared-api';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AppsModule } from './apps/apps.module';
 
 @Module({
   imports: [ShellModule, AppsModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
